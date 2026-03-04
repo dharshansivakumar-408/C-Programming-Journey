a885700d5796c7dc49206c8fd841c6c5c8f3e0ac
dharshansivakumar-408 <dharshansivakumar408@gmail.com>
2026-03-04 20:58:10 +0530

    Add program to create Simple calculator

---
+++ 01_Basics/simple_calculator.c
@@ -0,0 +1,15 @@
+#include"stdio.h"
+int main()
+{
+	int a,b;
+	printf("Enter A value:");
+	scanf("%d",&a);
+	printf("Enter B value:");
+	scanf("%d",&b);
+	printf("The addition value is:%d\n",a+b);
+	printf("The subtraction value is:%d\n",a-b);
+	printf("The division value is:%d\n",a%b);
+	printf("The multiplication value is:%d\n",a*b);
+	return 0;
+
+}

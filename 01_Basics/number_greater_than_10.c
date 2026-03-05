e2a2512763529a5a4e20e4d903c4939cb1a3300b
dharshansivakumar-408 <dharshansivakumar408@gmail.com>
2026-03-05 10:15:17 +0530

    Add program to check if number is greater than 10

---
+++ 01_Basics/number_greater_than_10.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+int main()
+{
+    int a;
+    printf("Enter A value:");
+    scanf("%d",&a);
+    if(a>10)
+     printf("Greater than 10");
+    else
+     printf("Less or equal to 10");
+    return 0;
+}

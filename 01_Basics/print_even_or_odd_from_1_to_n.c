93c71a9620fbdd49ce95498b75adc9f01b09abd9
Dharshan S <dharshansivakumar408@gmail.com>
2026-03-10 12:02:57 +0530

    Add program to print whether numbers from 1 to n are even or odd

---
+++ 01_Basics/print_even_or_odd_from_1_to_n.c
@@ -0,0 +1,13 @@
+#include"stdio.h"
+int main()
+{
+    int n=20;
+    for(int i=1;i<=20;i++)
+    {
+        if(i%2 == 0)
+         printf("%d - Even\n",i);
+        else
+         printf("%d - Odd\n",i);
+    }
+    return 0;
+}

a235cb2f6447200520b6eb2363429e4bf84e2fdc
Dharshan S <dharshansivakumar408@gmail.com>
2026-03-20 22:15:43 +0530

    Add a program to Swapping two number without third variable.c

---
+++ 01_Basics/Swapping_without_third_variable.c
@@ -0,0 +1,14 @@
+// Swapped numbers without using a temp variable!
+#include"stdio.h"
+int main()
+{
+    int p,q;
+    printf("Enter P value:");
+    scanf("%d",&p);
+    printf("Enter Q value:");
+    scanf("%d",&q);
+    printf("After swapping");
+    printf("\nEnter P value:%d",q);
+    printf("\nEnter Q value:%d",p);
+    
+}

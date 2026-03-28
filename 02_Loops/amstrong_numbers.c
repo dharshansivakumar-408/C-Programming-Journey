72107c7807d41a9f06409ecba83fbb2ab65ea9be
Dharshan S <dharshansivakumar408@gmail.com>
2026-03-28 22:44:03 +0530

    Adding a program to checj the  number is Armstrong or not

---
+++ 02_Loops/amstrong_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() 
+{
+    int n, originalNum, remainder, result = 0;
+    printf("Enter N integer: ");
+    scanf("%d", &n);
+    originalNum = n;
+    while (originalNum != 0) 
+    {
+        remainder = originalNum % 10;
+        result += remainder * remainder * remainder;
+        originalNum /= 10;
+    }
+    if (result == n)
+        printf("%d is an Armstrong number.\n", n);
+    else
+        printf("%d is not an Armstrong number.\n", n);
+
+    return 0;
+}

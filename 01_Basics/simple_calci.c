9c354a72a2daff6dd23c7e7262b6cb7b7989e3c4
Dharshan S <dharshansivakumar408@gmail.com>
2026-05-25 21:23:31 +0530

    Adding a program that calculate simple calculations

---
+++ 01_Basics/simple_calci.c
@@ -0,0 +1,10 @@
+#include"stdio.h"
+int main()
+{
+    int a=10,b=5;
+    printf("Addition value:%d",a+b);
+    printf("\nDifference value:%d",a-b);
+    printf("\nproduct value:%d",a*b);
+    printf("\nQuotient value:%d",a/b);
+    return 0;
+}
